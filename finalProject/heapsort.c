ae5a1ceb5c936d5afd040b3a93c6d584fcde70d7
s3xy m0f0 <jsc126@case.edu>
2017-12-05 22:59:27 -0500

    single threaded working

--- finalProject/heapsort.c
+++ finalProject/heapsort.c
@@ -1,34 +1,80 @@
 #include <stdio.h>
 
-void heapify(int array[]);
-void buildHeap(int array[]);
+void heapify(int array[], int index, int length);
 
 int sortArray(int values[], int length){
-	
+	int i;
+	//building the heap
+	for(i = 0; i < length-1; i++){
+		heapify(values, i, length);
+	}
+	//swap 0th element w/last, heapify
+	for(i = length-1; i >= 0; i--){
+		int swap = values[0];
+		values[0] = values[i];
+		values[i] = swap;
+		heapify(values, 0, i-1);
+	}
 }
 
 //creates heap in place,
 /*ith element:
-left child @ 2i
-right child @2i+1
-parent @ i/2*/
-void buildHeap(int array[]){
+  left child @ 2i
+  right child @2i+1
+  parent @ i/2*/
+void heapify(int array[], int index, int length){
+	if(index < length-1){
+	//left child
+	if(index % 2 == 0){
+		if(array[index+1] > array[(index+1)/2]){
+			//swap parent and current
+			int parent = array[(index+1)/2];
+			array[(index+1)/2] = array[index+1];
+			array[index+1] = parent;
+
+			int newIndex = (index+1)/2;
+			if(newIndex!= 0){
+				heapify(array, newIndex-1, length);
+			}
+		}
+	}
+	//right child
+	else{
+		if(array[index+1] > array[((index+1)/2)-1]){
+			int parent = array[((index+1)/2)-1];
+			array[((index+1)/2)-1] = array[index+1];
+			array[index+1] = parent;
+			int newIndex = ((index+1)/2)-1;
+			if(newIndex != 0){
+				heapify(array, newIndex-1, length);
+			}
+		}
+	}
+	}
 	
 }
 
-void heapify(int array[]){
-}
-
+//main driver of the code
 int main(void){
+	int values[10];
 	int index;
-	int values[128];
-	int len = 128;
-	int val = 128;
-
-	for(index = 0; index < len; index++){
-		values[index] = val;
-		val--;
+	for(index = 0 ; index < 10; index++){
+		values[index] = index;
 	}
+	sortArray(values, 10);
+	
+	for(index = 0; index < 10; index++){
+		printf("%i\n",values[index]);
+	}
+	/* int index; */
+	/* int values[128]; */
+	/* int len = 128; */
+	/* int val = 128; */
+
+	/* for(index = 0; index < len; index++){ */
+	/* 	values[index] = val; */
+	/* 	val--; */
+	/* } */
 
 	/* sortArray(values, len); */
 
--- finalProject/insertionSortOpenMp.c
+++ finalProject/insertionSortOpenMp.c
@@ -2,55 +2,149 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
-#include <omp.h>
 
-void sortArray(int values[], int length){
+float sortArray(int values[], int length){
 	struct timeval start_time, stop_time, elapsed_time;
 	int outerIndex;
 	int currVal;
 	gettimeofday(&start_time,NULL);
-
-#pragma omp parallel
-	{
-		for(outerIndex = 1 ; outerIndex < length; outerIndex++){
-			currVal = values[outerIndex];
-			int compareIndex = outerIndex -1;
-			while(currVal < values[compareIndex] && compareIndex >= 0){
-#pragma omp critical{
-				int temp = values[compareIndex];
-				values[compareIndex] = currVal;
-				values[compareIndex+1] = temp;
-				compareIndex--;
-			}
+	for(outerIndex = 1 ; outerIndex < length; outerIndex++){
+		currVal = values[outerIndex];
+		int compareIndex = outerIndex -1;
+		while(currVal < values[compareIndex] && compareIndex >= 0){
+			int temp = values[compareIndex];
+			values[compareIndex] = currVal;
+			values[compareIndex+1] = temp;
+			compareIndex--;
 		}
 	}
-}
 	gettimeofday(&stop_time,NULL);
-timersub(&stop_time, start_time, &elapsed_time);
-printf("Run time of insertion sort w/forking: %f", elapsed_time.tv_sec+elapsed_time.tv_usec/100000.0);
+	timersub(&stop_time, &start_time, &elapsed_time);
+	return elapsed_time.tv_sec+elapsed_time.tv_usec/100000.0;
 }
 
 int main(int argc, char *argv[]){
 	//creating the unsorted array
 	int temp;
 	int index;
-	int values[128];
-	int len = 128;
-	int val = 128;
-	
+	int len = 1000;
+
+	int *worstCase, *bestCase, *random;
+	worstCase = (int*)calloc(len, sizeof(int));
+	bestCase = (int*)calloc(len, sizeof(int));
+	random = (int*)calloc(len, sizeof(int));
+
+	int val = 1000;
 	for(index = 0 ; index < len; index++){
-		values[index] = val;
+		worstCase[index] = val;
 		val--;
 	}
 
-   	sortArray(values, len);
+	val = 1;
+	for(index = 0; index < len ; index++){
+		bestCase[index] = val;
+		val++;
+	}
+
+	srand(time(NULL));
+	for(index = 0; index < len; index++){
+		int r = rand();
+		random[index] = r;
+	}
+
+	float worstRunTime = sortArray(worstCase, len);
+	float bestRunTime = sortArray(bestCase, len);
+	float randomRunTime = sortArray(random, len);
 
 	int i;
-	for(i = 0 ; i < len; i++){
-		if(values[i] != i+1){
+	for(i = 1 ; i < len; i++){
+		if(worstCase[i] < worstCase[i-1] || bestCase[i] < bestCase[i-1] || random[i] < random[i-1]){
 			printf("Insertion sort failed to sort properly.");
 			return 0;
 		}
 	}
-	printf("Insertion sort success!");
+	printf("Insertion sort on backwards array len %i:, %f \n", len, worstRunTime);
+	printf("Insertion sort on sorted array len %i:, %f\n",len,  bestRunTime);
+	printf("Insertion sort on random array len %i:, %f\n", len, randomRunTime);
+
+	//running insertion sort on len 2000 arrays
+	len = 2000;
+
+	worstCase = (int*)calloc(len, sizeof(int));
+	bestCase = (int*)calloc(len, sizeof(int));
+	random = (int*)calloc(len, sizeof(int));
+
+	 val = 2000;
+	for(index = 0 ; index < len; index++){
+		worstCase[index] = val;
+		val--;
+	}
+
+	val = 1;
+	for(index = 0; index < len ; index++){
+		bestCase[index] = val;
+		val++;
+	}
+
+	srand(time(NULL));
+	for(index = 0; index < len; index++){
+		int r = rand();
+		random[index] = r;
+	}
+
+	 worstRunTime = sortArray(worstCase, len);
+	 bestRunTime = sortArray(bestCase, len);
+	 randomRunTime = sortArray(random, len);
+
+	 i;
+	for(i = 1 ; i < len; i++){
+		if(worstCase[i] < worstCase[i-1] || bestCase[i] < bestCase[i-1] || random[i] < random[i-1]){
+			printf("Insertion sort failed to sort properly.");
+			return 0;
+		}
+	}
+	printf("Insertion sort on backwards array len %i:, %f \n", len, worstRunTime);
+	printf("Insertion sort on sorted array len %i:, %f\n",len,  bestRunTime);
+	printf("Insertion sort on random array len %i:, %f\n", len, randomRunTime);
+
+		//running insertion sort on len 3000 arrays
+	len = 3000;
+
+	worstCase = (int*)calloc(len, sizeof(int));
+	bestCase = (int*)calloc(len, sizeof(int));
+	random = (int*)calloc(len, sizeof(int));
+
+	 val = 3000;
+	for(index = 0 ; index < len; index++){
+		worstCase[index] = val;
+		val--;
+	}
+
+	val = 1;
+	for(index = 0; index < len ; index++){
+		bestCase[index] = val;
+		val++;
+	}
+
+	srand(time(NULL));
+	for(index = 0; index < len; index++){
+		int r = rand();
+		random[index] = r;
+	}
+
+	 worstRunTime = sortArray(worstCase, len);
+	 bestRunTime = sortArray(bestCase, len);
+	 randomRunTime = sortArray(random, len);
+
+	 i;
+	for(i = 1 ; i < len; i++){
+		if(worstCase[i] < worstCase[i-1] || bestCase[i] < bestCase[i-1] || random[i] < random[i-1]){
+			printf("Insertion sort failed to sort properly.");
+			return 0;
+		}
+	}
+	printf("Insertion sort on backwards array len %i:, %f \n", len, worstRunTime);
+	printf("Insertion sort on sorted array len %i:, %f\n",len,  bestRunTime);
+	printf("Insertion sort on random array len %i:, %f\n", len, randomRunTime);
+	
 }
